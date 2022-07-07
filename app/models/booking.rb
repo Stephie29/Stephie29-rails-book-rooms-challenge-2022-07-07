18eee0e36648f6ac139be8de43afd146cc7250e9
stephanie joseph <jos.stephanie@gmail.com>
2022-07-07 17:12:05 +0200

    models validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  validates :starts_at, :ends_at, presence: true
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,5 @@
 class Hotel < ApplicationRecord
   has_many :rooms, dependent: :destroy
   has_many :bookings, through: :rooms
+  validates :name, :address, presence: true
 end
